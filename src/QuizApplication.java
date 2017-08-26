2dcb7ff21c3b8be2500e83dd3b658c38150afa5a
Dennis Grabowski <dennisgrabowski@gmx.de>
2017-08-26 20:23:53 +0200

    core: add main entry point

---
+++ src/QuizApplication.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class QuizApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+
+    public static void main(String[] args) {
+        QuizApplication.launch(args);
+    }
+}
