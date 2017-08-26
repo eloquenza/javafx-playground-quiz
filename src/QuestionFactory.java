c577648a8e1699f97af7f49520933fa556756bde
Dennis Grabowski <dennisgrabowski@gmx.de>
2017-08-26 20:14:47 +0200

    core: add factory class to create questions of varying topics

---
+++ src/QuestionFactory.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class QuestionFactory {
+
+    public enum Topics {
+        HISTORY
+    }
+
+    public static ArrayList<Question> createQuestions(Topics topic) {
+        ArrayList<Question> list = createQuestionsImpl(topic);
+        return list;
+    }
+
+    private static ArrayList<Question> createQuestionsImpl(Topics topic) {
+        switch (topic) {
+            case HISTORY:
+                return HistoryQuestionsFactory.createQuestions();
+        }
+        return new ArrayList<Question>();
+    }
+}
