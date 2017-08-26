fc4b5bb6eeb196810e4c54062c57c5229cd19698
Dennis Grabowski <dennisgrabowski@gmx.de>
2017-08-26 20:24:33 +0200

    core: add controller to hold relevant data and control dialogs

---
+++ src/Controller.java
@@ -0,0 +1,48 @@
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Controller {
+
+    Stage st;
+
+    TopicChoicerController tcp;
+    QuestionController qc;
+
+    FXMLLoader topicLoader;
+    FXMLLoader questionLoader;
+
+    Scene scene;
+
+    BorderPane topics;
+    BorderPane questions;
+
+    Node topicNode;
+    Node questionNode;
+
+
+    // Pay attention to volatile
+    private static volatile Controller INSTANCE = null;
+
+    private Controller() {
+    }
+
+    public static Controller getInstance() {
+        if (INSTANCE == null) { // Check 1
+            synchronized (Controller.class) {
+                if (INSTANCE == null) { // Check 2
+                    INSTANCE = new Controller();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
--- src/QuizApplication.java
+++ src/QuizApplication.java
@@ -7,6 +7,29 @@ public class QuizApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Controller cont = Controller.getInstance();
+        cont.st = stage;
+
+        cont.topicLoader = new FXMLLoader(getClass().getResource("TopicsGUI.fxml"));
+        cont.questionLoader = new FXMLLoader(getClass().getResource("QuestionsGUI.fxml"));
+
+        cont.tcp = new TopicChoicerController();
+        cont.qc = new QuestionController();
+
+        cont.topicLoader.setController(cont.tcp);
+        cont.questionLoader.setController(cont.qc);
+
+        cont.topics = cont.topicLoader.load();
+        cont.questions = cont.questionLoader.load();
+
+        cont.topicNode = cont.topics.getCenter();
+        cont.questionNode = cont.questions.getCenter();
+
+        cont.scene = new Scene(cont.topics);
+
+        cont.st.setTitle("Quiz");
+        cont.st.setScene(cont.scene);
+        cont.st.show();
     }
 
     public static void main(String[] args) {
