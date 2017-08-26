046d13c5ec4b229ced3556baa75216b6d51c6ebf
Dennis Grabowski <dennisgrabowski@gmx.de>
2017-08-26 20:10:43 +0200

    core: add question entity to hold relevant data about a question
    Answers as a List<String> so we could shuffle the answers later, if we
    want to ensure that the answers will be placed on different buttons all
    the time.

---
+++ src/Question.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Question {
+
+    public String qstn;
+    public List<String> answers;
+    public String right_answer;
+
+    public Question(String question, String[] answers) {
+        qstn = question;
+        right_answer = answers[0];
+        this.answers = Arrays.asList(answers);
+    }
+}
